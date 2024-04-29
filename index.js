392007661de74f5f73ece4fc983e970d55207b6a
simran9630 <simran@optacloud.in>
2024-04-29 15:03:39 +0530

    yaml modified 13

--- index.js
+++ index.js
@@ -1,1 +1,65 @@
-console.log("abcdef");
+const axios = require('axios');
+require('dotenv').config()
+// Function to fetch weather data from OpenWeatherMap API
+async function fetchWeatherData(lat, lon, apiKey) {
+    try {
+        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`);
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching weather data:', error);
+        throw error;
+    }
+}
+
+// Function to call another API if temperature is greater than 30
+async function processWeatherData(lat, lon, apiKey) {
+    try {
+        const weatherData = await fetchWeatherData(lat, lon, apiKey);
+        console.log(weatherData)
+        const temperature = weatherData.main.temp; // Temperature in Kelvin
+        if (temperature > 303.15) { // Convert Kelvin to Celsius
+            // Call another API here
+            console.log('Temperature is greater than 30°C. Calling another API...');
+            // Call your other API here
+        } else {
+            console.log('Temperature is not greater than 30°C.');
+        }
+    } catch (error) {
+        console.error('Error processing weather data:', error);
+    }
+}
+
+async function runbamboo() {
+    axios.post(`http://localhost:8085/rest/api/latest/queue/${'DEM'}-TES`,{}, {
+        headers: {
+            
+            'Authorization':`Bearer ${process.env.AUTH_TOKEN}`
+        }
+    })
+    .then(response => {
+        console.log(`Response: ${response.status} ${response.statusText}`);
+        console.log(response.data); 
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+}
+
+
+// Example usage
+const latitude = 37.7749; // Example latitude
+const longitude = -122.4194; // Example longitude
+const apiKey = process.env.API_KEY;
+
+const express = require('express');
+
+const app = express();
+const PORT = 3000; // Define the port number
+
+
+app.listen(PORT, () => {
+    // processWeatherData(latitude, longitude, apiKey);
+    runbamboo();
+    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(apiKey);
+});
