b3440b8df484fff44c304c8dde4226b2f7e41966
simran9630 <simran@optacloud.in>
2024-05-06 10:33:33 +0530

    finall

--- index.js
+++ index.js
@@ -50,8 +50,8 @@ async function fetchWeatherData(lat, lon, apiKey) {
 
 async function createRelease() {
     axios.post(`http://localhost:8085/rest/api/latest/deploy/project/11010049/version`,{
-        name:"release-8",
-        nextVersionName:"release-9",
+        name:"release-10",
+        nextVersionName:"release-11",
         planResultKey:"DEM-TES1-73"
     },{
         headers: {
